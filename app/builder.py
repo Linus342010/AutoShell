000cb5af52c0afefa8b4bc9cd7271181de5a2d4b
Shadow <shadows.mail@icloud.com>
2026-03-30 18:10:09 +0200

    Update builder.py

--- app/builder.py
+++ app/builder.py
@@ -8,6 +8,29 @@ class window(QMainWindow):
         self.ListWidgetLeft = QListWidget()
         self.ListWidgetRight = QListWidget()
 
-        self.ListWidgetLeft(True)
-        self.ListWidgetRight(True)
+        self.ListWidgetLeft.setAcceptDrops(True)
+        self.ListWidgetLeft.setDragEnabled(True)
+        self.ListWidgetRight.setAcceptDrops(True)
+        self.ListWidgetRight.setDragEnabled(True)
+
+        self.setGeometry(300, 350, 800, 400)
+
+        self.hboxlayout = QHBoxLayout()
+        self.hboxlayout.addWidget(self.ListWidgetLeft)
+        self.hboxlayout.addWidget(self.ListWidgetRight)
+
+        l1 = QListWidgetItem("Command 1")
+        l2 = QListWidgetItem("Command 2")
+
+        self.ListWidgetLeft.insertItem(1, l1)
+        self.ListWidgetLeft.insertItem(2, l2)
+
+
+        self.setWindowTitle("AutoShell Drag&Drop Builder")
+        self.setLayout(self.hboxlayout)
+        self.show()
+        
+app = QApplication(sys.argv)
+builder = window()
+app.exec()
 
